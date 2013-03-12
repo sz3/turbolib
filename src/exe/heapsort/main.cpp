d69b34b14fc47680dbca44d359d0cdc598638fcd
sz <sz@galacticicecube.com>
2013-03-12 03:39:42 +0000

    Getting smoked by qsort... :(

--- src/exe/heapsort/main.cpp
+++ src/exe/heapsort/main.cpp
@@ -1,18 +1,38 @@
 #include "heapsort.h"
 
+#include "event/Timer.h"
 #include <iostream>
 using namespace std;
 
-#define BUFSIZE 1500
+#define BUFSIZE 30000
+
+int compareMyType(const void* a, const void* b)
+{
+	if ( *(unsigned*)a <  *(unsigned*)b ) return -1;
+	if ( *(unsigned*)a == *(unsigned*)b ) return 0;
+	if ( *(unsigned*)a >  *(unsigned*)b ) return 1;
+}
 
 int main(int argc, char** argv)
 {
-	int buffer[BUFSIZE];
+	unsigned* buffer = new unsigned[BUFSIZE];
 	for (unsigned i = 0; i < BUFSIZE; ++i)
-		buffer[i] = BUFSIZE-i;
+		buffer[i] = i;
 
+	Timer t;
 	heapsort(buffer, BUFSIZE);
+	long long elapsed = t.micros();
+	cout << elapsed << "us" << endl;
+
 	for (unsigned i = 0; i < BUFSIZE; ++i)
+		buffer[i] = i;
+
+	Timer t2;
+	qsort(buffer, BUFSIZE, sizeof(unsigned), &compareMyType);
+	long long el2 = t2.micros();
+	cout << el2 << "us" << endl;
+
+	/*for (unsigned i = 0; i < BUFSIZE; ++i)
 	{
 		if (i % 20 == 0)
 			cout << endl;
@@ -20,6 +40,8 @@ int main(int argc, char** argv)
 			cout << ",";
 		cout << buffer[i];
 	}
-	cout << endl;
+	cout << endl;*/
+
+	delete[] buffer;
 	return 0;
 }
