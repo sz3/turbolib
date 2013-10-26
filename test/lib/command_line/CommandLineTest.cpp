3d496a7068e56e3bbedbff69f3dabc2d9560012d
sz <sz@galacticicecube.com>
2013-10-26 04:20:09 +0000

    CommandLine::run() needs to read stdout multiple times, since bash does exciting things with the linebuffer.

--- test/lib/command_line/CommandLineTest.cpp
+++ test/lib/command_line/CommandLineTest.cpp
@@ -5,7 +5,8 @@ using std::string;
 
 TEST_CASE( "CommandLineTest/testRun", "description" )
 {
-	string result = CommandLine::run("ls");
-	REQUIRE( result == "CMakeFiles\n" );
+	string result = CommandLine::run("ls | grep cmake");
+	REQUIRE( "cmake_install.cmake\n"
+			 "CTestTestfile.cmake\n" == result );
 }
 
