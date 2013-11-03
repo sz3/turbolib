27c937d4deb01daac2fbc087ebced33d5fe45f0a
sz <sz@galacticicecube.com>
2013-11-03 02:58:35 +0000

    More merkley stuff, untested.

--- test/lib/util/merkle_treeTest.cpp
+++ test/lib/util/merkle_treeTest.cpp
@@ -3,6 +3,13 @@
 #include "merkle_tree.h"
 #include <iostream>
 
+// two parties: initiator (I) and responder (R)
+// I: ping|hash=0xf00
+// R: merkle_resp|level=0|[1024 bytes]
+// I: merkle_req|level=1 prefix=1337|
+// R: merkle_resp|level=1 prefix=1337|[1024 bytes]
+// I: file_req|name=foo|
+
 TEST_CASE( "merkle_treeTest/testDefault", "[unit]" )
 {
 	merkle_tree<unsigned, unsigned> merk;
