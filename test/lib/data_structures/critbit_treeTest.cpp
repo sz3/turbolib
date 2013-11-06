bf7c38d5646c238461944f97c62e038493e8808d
sz <sz@galacticicecube.com>
2013-11-06 02:09:47 +0000

    critbit tree can now handle both char* AND standard object types (provided the complex type has an [operator const uint8_t*])

--- test/lib/data_structures/critbit_treeTest.cpp
+++ test/lib/data_structures/critbit_treeTest.cpp
@@ -2,10 +2,32 @@
 
 #include "critbit_tree.h"
 #include <iostream>
+#include <string>
+using std::string;
+
+namespace {
+	class Foo
+	{
+	public:
+		Foo(const string& str, const string& payload = "")
+			: _str(str)
+			, _payload(payload)
+		{}
+
+		operator const uint8_t*() const
+		{
+			return (const uint8_t*)_str.c_str();
+		}
+
+	public:
+		string _str;
+		string _payload;
+	};
+}
 
 TEST_CASE( "critbit_treeTest/testInsertLookup", "[unit]" )
 {
-	critbit_tree<char> tree;
+	critbit_tree<char, const char*> tree;
 
 	std::cout << "insert? " << tree.insert("one") << std::endl;
 	std::cout << "find? " << (unsigned)tree.find("one") << std::endl;
@@ -19,5 +41,28 @@ TEST_CASE( "critbit_treeTest/testInsertLookup", "[unit]" )
 
 	assertEquals(1, tree.insert("one") );
 	assertTrue( tree.contains("one") );
+}
+
+TEST_CASE( "critbit_treeTest/testClass", "[unit]" )
+{
+	critbit_tree<Foo> tree;
+
+	Foo turtle("turtle", "banana");
+	assertEquals( 2, tree.insert(turtle) );
+	assertTrue( tree.contains(Foo("turtle")) );
+	assertEquals( "banana", tree.find(Foo("turtle"))->_payload );
+
+	Foo monkey("monkey", "orange");
+	assertEquals(2, tree.insert(monkey) );
+	assertTrue( tree.contains(Foo("monkey")) );
+	assertEquals( "orange", tree.find(Foo("monkey"))->_payload );
+
+	Foo fortyseven("forty-seven", "rocketship");
+	assertEquals(2, tree.insert(fortyseven) );
+	assertTrue( tree.contains(Foo("forty-seven")) );
+	assertEquals( "rocketship", tree.find(Foo("forty-seven"))->_payload );
+
+	assertEquals(1, tree.insert(Foo("turtle")) );
+	assertTrue( tree.contains(Foo("turtle")) );
 }
 
