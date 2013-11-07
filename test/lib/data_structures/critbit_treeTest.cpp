274d64ecef1f5335ce3ddc48f2ecdb625746d1c3
sz <sz@galacticicecube.com>
2013-11-07 20:49:15 +0000

    Adding critbit_ext, a templated thingy to hopefully enable merkle-age

--- test/lib/data_structures/critbit_treeTest.cpp
+++ test/lib/data_structures/critbit_treeTest.cpp
@@ -13,7 +13,7 @@ TEST_CASE( "critbit_treeTest/testInsertLookup", "[unit]" )
 	critbit_tree<char, const char*> tree;
 
 	std::cout << "insert? " << tree.insert("one") << std::endl;
-	std::cout << "find? " << (unsigned)tree.find("one") << std::endl;
+	std::cout << "find? " << (unsigned)tree.lower_bound("one") << std::endl;
 	std::cout << "contains? " << tree.contains("one") << std::endl;
 
 	assertEquals(2, tree.insert("two") );
@@ -84,6 +84,11 @@ namespace {
 			return (const uint8_t*)_str.c_str();
 		}
 
+		bool operator==(const KeyString& right) const
+		{
+			return _str == right._str;
+		}
+
 		static size_t key_size(const uint8_t* cstr)
 		{
 			return strlen((const char*)cstr);
@@ -101,15 +106,15 @@ TEST_CASE( "critbit_treeTest/testClassKeyCstr", "[unit]" )
 
 	assertEquals( 2, tree.insert(KeyString("turtle", "banana")) );
 	assertTrue( tree.contains(KeyString("turtle")) );
-	assertEquals( "banana", tree.find(KeyString("turtle"))->_payload );
+	assertEquals( "banana", tree.lower_bound(KeyString("turtle"))->_payload );
 
 	assertEquals(2, tree.insert(KeyString("monkey", "orange")) );
 	assertTrue( tree.contains(KeyString("monkey")) );
-	assertEquals( "orange", tree.find(KeyString("monkey"))->_payload );
+	assertEquals( "orange", tree.lower_bound(KeyString("monkey"))->_payload );
 
 	assertEquals(2, tree.insert(KeyString("forty-seven", "rocketship")) );
 	assertTrue( tree.contains(KeyString("forty-seven")) );
-	assertEquals( "rocketship", tree.find(KeyString("forty-seven"))->_payload );
+	assertEquals( "rocketship", tree.lower_bound(KeyString("forty-seven"))->_payload );
 
 	assertEquals(1, tree.insert(KeyString("turtle")) );
 	assertTrue( tree.contains(KeyString("turtle")) );
@@ -130,7 +135,7 @@ TEST_CASE( "critbit_treeTest/testClassKeyCstr_Load", "[unit]" )
 		std::stringstream ss;
 		ss << i;
 		assertEquals( 2, tree.insert(KeyString(ss.str(), "banana")) );
-		KeyString* internalNode = tree.find(ss.str());
+		KeyString* internalNode = tree.lower_bound(ss.str());
 		assertNotNull(internalNode);
 		assertEquals( ss.str(), internalNode->_str );
 		assertEquals( "banana", internalNode->_payload );
@@ -156,7 +161,7 @@ TEST_CASE( "critbit_treeTest/testClassKeyCstr_Random", "[unit]" )
 
 	for (std::vector<string>::const_iterator it = words.begin(); it != words.end(); ++it)
 	{
-		KeyString* internalNode = tree.find(*it);
+		KeyString* internalNode = tree.lower_bound(*it);
 		assertNotNull(internalNode);
 		assertEquals(*it, internalNode->_payload);
 	}
@@ -176,6 +181,11 @@ namespace {
 			return (const uint8_t*)&_key;
 		}
 
+		bool operator==(const KeyInt& right) const
+		{
+			return _key == right._key;
+		}
+
 		static size_t key_size(const uint8_t*)
 		{
 			return sizeof(unsigned);
@@ -193,15 +203,15 @@ TEST_CASE( "critbit_treeTest/testClassKeyInt", "[unit]" )
 
 	assertEquals( 2, tree.insert(KeyInt(10, "banana")) );
 	assertTrue( tree.contains(10) );
-	assertEquals( "banana", tree.find(10)->_payload );
+	assertEquals( "banana", tree.lower_bound(10)->_payload );
 
 	assertEquals(2, tree.insert(KeyInt(20, "orange")) );
 	assertTrue( tree.contains(20) );
-	assertEquals( "orange", tree.find(20)->_payload );
+	assertEquals( "orange", tree.lower_bound(20)->_payload );
 
 	assertEquals(2, tree.insert(KeyInt(47, "rocketship")) );
 	assertTrue( tree.contains(47) );
-	assertEquals( "rocketship", tree.find(47)->_payload );
+	assertEquals( "rocketship", tree.lower_bound(47)->_payload );
 
 	assertEquals(1, tree.insert(10) );
 	assertTrue( tree.contains(10) );
@@ -221,13 +231,13 @@ TEST_CASE( "critbit_treeTest/testClassKeyInt_Load", "[unit]" )
 	{
 		assertEquals( 2, tree.insert(KeyInt(i, "banana")) );
 
-		KeyInt* internalNode = tree.find(i);
+		KeyInt* internalNode = tree.lower_bound(i);
 		assertNotNull( internalNode );
 		assertEquals( i, internalNode->_key );
 		assertEquals( "banana", internalNode->_payload );
 	}
 
-	KeyInt* node = tree.find(10);
+	KeyInt* node = tree.lower_bound(10);
 	assertNotNull( node );
 	assertEquals( 10, node->_key );
 	assertEquals( "banana", node->_payload );
