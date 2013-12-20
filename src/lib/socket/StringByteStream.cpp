3ab6ffd2eecc0c4431d6040c5f286bb9f5f49cae
sz <sz@galacticicecube.com>
2013-12-20 23:17:06 +0000

    Adding a test suit for Switchboard (finally). Debating the merits of altogether switching him to HTTP.

---
+++ src/lib/socket/StringByteStream.cpp
@@ -0,0 +1,46 @@
+#include "StringByteStream.h"
+
+#include <cstring>
+
+StringByteStream::StringByteStream(unsigned packetLength)
+	: _packetLength(packetLength)
+{
+}
+
+StringByteStream::StringByteStream(const std::string& readBuffer, unsigned packetLength)
+	: _packetLength(packetLength)
+	, _readBuffer(readBuffer)
+{
+}
+
+unsigned StringByteStream::maxPacketLength() const
+{
+	return _packetLength;
+}
+
+int StringByteStream::read(char* buffer, unsigned length)
+{
+	unsigned bytes = std::min(_readBuffer.size(), length);
+	if (bytes == 0)
+		return 0;
+
+	strncpy(buffer, _readBuffer.data(), bytes);
+	_readBuffer = _readBuffer.substr(bytes);
+	return bytes;
+}
+
+int StringByteStream::write(const char* buffer, unsigned length)
+{
+	_writeBuffer.append( std::string(buffer,length) );
+	return length;
+}
+
+const std::string& StringByteStream::readBuffer() const
+{
+	return _readBuffer;
+}
+
+const std::string& StringByteStream::writeBuffer() const
+{
+	return _writeBuffer;
+}
---
+++ src/lib/socket/StringByteStream.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "IByteStream.h"
+#include <string>
+
+class StringByteStream : public IByteStream
+{
+public:
+	StringByteStream(unsigned packetLength=1024);
+	StringByteStream(const std::string& str, unsigned packetLength=1024);
+
+	unsigned maxPacketLength() const;
+
+	int read(char* buffer, unsigned length);
+	int write(const char* buffer, unsigned length);
+
+	const std::string& readBuffer() const;
+	const std::string& writeBuffer() const;
+
+protected:
+	unsigned _packetLength = 1024;
+	std::string _readBuffer;
+	std::string _writeBuffer;
+};
