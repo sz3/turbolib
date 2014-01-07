ace6a9d586fff8807616df8db84d9d1767cd1477
sz <sz@galacticicecube.com>
2014-01-07 17:28:35 -0600

    License attributions

--- src/lib/data_structures/critbit_tree.h
+++ src/lib/data_structures/critbit_tree.h
@@ -1,3 +1,4 @@
+/* This code is subject to the terms of the Mozilla Public License, v.2.0. http://mozilla.org/MPL/2.0/. */
 /*
 adapted from DJB's critbit tree for NULL-terminated strings
 	see: http://cr.yp.to/critbit.html
