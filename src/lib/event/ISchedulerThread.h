3a213a20ae38ed4badae94684d3e2c20c6bc4df1
sz <sz@galacticicecube.com>
2014-01-20 19:14:58 -0600

    Extra include

--- src/lib/event/ISchedulerThread.h
+++ src/lib/event/ISchedulerThread.h
@@ -1,6 +1,8 @@
 /* This code is subject to the terms of the Mozilla Public License, v.2.0. http://mozilla.org/MPL/2.0/. */
 #pragma once
 
+#include <functional>
+
 class ISchedulerThread
 {
 public:
