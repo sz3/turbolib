930a5403688dc94d89aefa5173840148b910d7c8
sz <sz@galacticicecube.com>
2014-06-07 00:16:37 -0500

    Special mutex lock guards.

---
+++ src/lib/mutex/conditional_lock_guard.h
@@ -0,0 +1,25 @@
+/* This code is subject to the terms of the Mozilla Public License, v.2.0. http://mozilla.org/MPL/2.0/. */
+#pragma once
+
+template <typename M>
+class conditional_lock_guard
+{
+public:
+	conditional_lock_guard(M& myMutex, bool shouldLock)
+		: _mutex(myMutex)
+		, _shouldLock(shouldLock)
+	{
+		if (_shouldLock)
+			_mutex.lock();
+	}
+
+	~conditional_lock_guard()
+	{
+		if (_shouldLock)
+			_mutex.unlock();
+	}
+
+protected:
+	M& _mutex;
+	bool _shouldLock;
+};
---
+++ src/lib/mutex/unlock_guard.h
@@ -0,0 +1,21 @@
+/* This code is subject to the terms of the Mozilla Public License, v.2.0. http://mozilla.org/MPL/2.0/. */
+#pragma once
+
+template <typename M>
+class unlock_guard
+{
+public:
+	unlock_guard(M& myMutex)
+		: _mutex(myMutex)
+	{
+		_mutex.unlock();
+	}
+
+	~unlock_guard()
+	{
+		_mutex.lock();
+	}
+
+protected:
+	M& _mutex;
+};
