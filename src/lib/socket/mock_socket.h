b296b31b4c83b75f3b7707257e24e0693572b887
sz <sz@galacticicecube.com>
2014-11-08 20:00:46 -0600

    Improving mock_socket.

--- src/lib/socket/mock_socket.h
+++ src/lib/socket/mock_socket.h
@@ -2,13 +2,17 @@
 
 #include "socket_address.h"
 #include "util/CallHistory.h"
+#include <memory>
 #include <string>
 
 class mock_socket
 {
 public:
 	mock_socket(std::string endpoint)
-		: _endpoint(endpoint)
+		: _history(new CallHistory())
+		, _endpoint(endpoint)
+		, _trySendError(false)
+		, _trySendErrorBytes(-1)
 	{}
 
 	std::string target() const
@@ -23,23 +27,33 @@ public:
 
 	int try_send(const char* buffer, unsigned size)
 	{
-		_history.call("try_send", std::string(buffer, size));
+		_history->call("try_send", std::string(buffer, size));
+		if (_trySendError)
+			return _trySendErrorBytes;
 		return size;
 	}
 
 	int send(const char* buffer, unsigned size)
 	{
-		_history.call("send", std::string(buffer, size));
+		_history->call("send", std::string(buffer, size));
 		return size;
 	}
 
 	bool close()
 	{
-		_history.call("close");
+		_history->call("close");
 		return true;
 	}
 
+	CallHistory& history()
+	{
+		return *_history;
+	}
+
 public:
-	CallHistory _history;
+	std::shared_ptr<CallHistory> _history;
 	socket_address _endpoint;
+
+	bool _trySendError;
+	int _trySendErrorBytes;
 };
