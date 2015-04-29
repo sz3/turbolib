642605f557fc3b774c1b6f1bc41a551667d37f5e
sz <sz@galacticicecube.com>
2015-04-28 21:59:50 -0500

    SchedulerThread -> deadline_thread, now with less virtual.

---
+++ src/lib/concurrent/deadline_thread.h
@@ -0,0 +1,142 @@
+/* This code is subject to the terms of the Mozilla Public License, v.2.0. http://mozilla.org/MPL/2.0/. */
+#pragma once
+
+#include "mutex/monitor.h"
+#include <atomic>
+#include <chrono>
+#include <functional>
+#include <mutex>
+#include <queue>
+#include <thread>
+
+namespace turbo {
+class deadline_thread
+{
+protected:
+	struct job
+	{
+		std::chrono::system_clock::time_point time;
+		std::function<void()> fun;
+
+		bool operator<(const job& other) const
+		{
+			return time > other.time;
+		}
+	};
+
+	static std::chrono::system_clock::time_point now()
+	{
+		return std::chrono::system_clock::now();
+	}
+
+	static long long millisFromNow(const std::chrono::system_clock::time_point& when)
+	{
+		return std::chrono::duration_cast<std::chrono::milliseconds>(when - now()).count();
+	}
+
+public:
+	deadline_thread();
+	~deadline_thread();
+
+	void shutdown();
+
+	void schedule(const std::function<void()>& fun, unsigned ms);
+	void schedule_repeat(const std::function<void()>& fun, unsigned ms);
+
+protected:
+	void run();
+
+	void waitForWork();
+	bool doWork();
+
+protected:
+	std::atomic<bool> _shutdown;
+	std::thread _thread;
+
+	std::mutex _mutex;
+	std::priority_queue<job> _jobs;
+	turbo::monitor _notifyWork;
+};
+
+inline deadline_thread::deadline_thread()
+	: _shutdown(false)
+	, _thread(std::bind(&deadline_thread::run, this))
+{
+}
+
+inline deadline_thread::~deadline_thread()
+{
+	shutdown();
+}
+
+inline void deadline_thread::shutdown()
+{
+	if (_shutdown.exchange(true))
+		return;
+	_notifyWork.notify_all();
+	_thread.join();
+}
+
+inline void deadline_thread::run()
+{
+	while (!_shutdown)
+	{
+		waitForWork();
+		while (doWork());
+	}
+}
+
+inline void deadline_thread::waitForWork()
+{
+	unsigned interval = ~0;
+	{
+		std::lock_guard<std::mutex> lock(_mutex);
+		if (!_jobs.empty())
+		{
+			const job& top = _jobs.top();
+			long long duration = millisFromNow(top.time);
+			interval = (duration < 0)? 0 : duration;
+		}
+	}
+	_notifyWork.wait_for(interval);
+}
+
+inline bool deadline_thread::doWork()
+{
+	if (_shutdown)
+		return false;
+
+	std::function<void()> work;
+	{
+		std::lock_guard<std::mutex> lock(_mutex);
+		if (_jobs.empty())
+			return false;
+
+		const job& top = _jobs.top();
+		if (millisFromNow(top.time) > 0) // would like to use operator<, but apparently it doesn't work?
+			return false;
+
+		work = top.fun;
+		_jobs.pop();
+	}
+	work();
+	return true;
+}
+
+inline void deadline_thread::schedule(const std::function<void()>& fun, unsigned ms)
+{
+	std::chrono::system_clock::time_point when = now() + std::chrono::milliseconds(ms);
+	{
+		std::lock_guard<std::mutex> lock(_mutex);
+		_jobs.push( {when, fun} );
+	}
+	_notifyWork.notify_one();
+}
+
+inline void deadline_thread::schedule_repeat(const std::function<void()>& fun, unsigned ms)
+{
+	std::function<void()> wrapper( [=] () { fun(); this->schedule_repeat(fun, ms); } );
+	schedule(wrapper, ms);
+}
+
+}//namespace
--- test/lib/concurrent/deadline_threadTest.cpp
+++ test/lib/concurrent/deadline_threadTest.cpp
@@ -1,43 +1,45 @@
 /* This code is subject to the terms of the Mozilla Public License, v.2.0. http://mozilla.org/MPL/2.0/. */
 #include "unittest.h"
 
-#include "SchedulerThread.h"
+#include "deadline_thread.h"
 
-#include "Event.h"
-#include "time/stopwatch.h"
+#include "mutex/monitor.h"
 #include "serialize/str_join.h"
+#include "time/stopwatch.h"
 #include <vector>
 using std::bind;
 using std::vector;
+using turbo::deadline_thread;
+using turbo::monitor;
 using turbo::stopwatch;
 
-TEST_CASE( "SchedulerThreadTest/testSchedule", "default" )
+TEST_CASE( "deadline_threadTest/testSchedule", "default" )
 {
-	SchedulerThread scheduler;
+	deadline_thread scheduler;
 
 	stopwatch time;
 	vector<unsigned> results;
 	vector<unsigned> timing;
-	Event midway;
-	Event finished;
+	monitor midway;
+	monitor finished;
 
-	scheduler.schedule( bind(&Event::signal, &finished), 150 );
+	scheduler.schedule( bind(&monitor::notify_all, &finished), 150 );
 	scheduler.schedule( [&] () { results.push_back(5); }, 5 );
 	scheduler.schedule( [&] () { timing.push_back(time.millis()); }, 5 );
 	scheduler.schedule( [&] () { results.push_back(50); }, 50 );
 	scheduler.schedule( [&] () { timing.push_back(time.millis()); }, 50 );
-	scheduler.schedule( bind(&Event::signal, &midway), 70 );
+	scheduler.schedule( bind(&monitor::notify_all, &midway), 70 );
 	scheduler.schedule( [&] () { results.push_back(100); }, 100 );
 	scheduler.schedule( [&] () { timing.push_back(time.millis()); }, 100 );
 
-	assertTrue( midway.wait(700) );
+	assertTrue( midway.wait_for(700) );
 
 	scheduler.schedule( [&] () { results.push_back(100); }, 30 );
 	scheduler.schedule( [&] () { timing.push_back(time.millis()); }, 30 );
 	scheduler.schedule( [&] () { results.push_back(130); }, 60 );
 	scheduler.schedule( [&] () { timing.push_back(time.millis()); }, 60 );
 
-	assertTrue( finished.wait(1000) );
+	assertTrue( finished.wait_for(1000) );
 	assertEquals( "5 50 100 100 130", turbo::str::join(results) );
 
 	assertEquals(5, timing.size());
@@ -48,21 +50,21 @@ TEST_CASE( "SchedulerThreadTest/testSchedule", "default" )
 	assertInRange(127, 133, timing[4]);
 }
 
-TEST_CASE( "SchedulerThreadTest/testPeriodic", "default" )
+TEST_CASE( "deadline_threadTest/testPeriodic", "default" )
 {
-	SchedulerThread scheduler;
+	deadline_thread scheduler;
 
 	stopwatch time;
 	vector<unsigned> results;
 	vector<unsigned> timing;
-	Event finished;
+	monitor finished;
 
-	scheduler.schedule( bind(&Event::signal, &finished), 100 );
+	scheduler.schedule( bind(&monitor::notify_all, &finished), 100 );
 
-	scheduler.schedulePeriodic( [&] () { results.push_back(30); }, 30 );
-	scheduler.schedulePeriodic( [&] () { timing.push_back(time.millis()); }, 30 );
+	scheduler.schedule_repeat( [&] () { results.push_back(30); }, 30 );
+	scheduler.schedule_repeat( [&] () { timing.push_back(time.millis()); }, 30 );
 
-	assertTrue( finished.wait(1000) );
+	assertTrue( finished.wait_for(1000) );
 	assertEquals( "30 30 30", turbo::str::join(results) );
 
 	assertEquals(3, timing.size());
