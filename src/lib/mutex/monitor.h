0a8fb672d5d2e6811c6ec37561fd4ec13a9eaf00
sz <sz@galacticicecube.com>
2015-04-29 21:22:08 -0500

    No need for atomic since we have a mutex anyway... and an extra method for flexibility.

--- src/lib/mutex/monitor.h
+++ src/lib/mutex/monitor.h
@@ -1,7 +1,6 @@
 /* This code is subject to the terms of the Mozilla Public License, v.2.0. http://mozilla.org/MPL/2.0/. */
 #pragma once
 
-#include <atomic>
 #include <condition_variable>
 #include <mutex>
 
@@ -16,16 +15,22 @@ public:
 	void wait()
 	{
 		std::unique_lock<std::mutex> lock(_mutex);
-		if (_flag.exchange(false))
+		if (_flag)
+		{
+			_flag = false;
 			return;
+		}
 		_cv.wait(lock);
 	}
 
 	bool wait_for(unsigned ms)
 	{
 		std::unique_lock<std::mutex> lock(_mutex);
-		if (_flag.exchange(false))
+		if (_flag)
+		{
+			_flag = false;
 			return true;
+		}
 		return _cv.wait_for(lock, std::chrono::milliseconds(ms)) != std::cv_status::timeout;
 	}
 
@@ -36,13 +41,18 @@ public:
 
 	void notify_all()
 	{
-		std::unique_lock<std::mutex> lock(_mutex);
-		_flag.store(true);
 		_cv.notify_all();
 	}
 
+	void signal_all()
+	{
+		std::unique_lock<std::mutex> lock(_mutex);
+		_flag = true;
+		notify_all();
+	}
+
 protected:
-	std::atomic<bool> _flag;
+	bool _flag;
 	std::condition_variable _cv;
 	std::mutex _mutex;
 };
