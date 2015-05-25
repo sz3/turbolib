4edf77fd6ed8ecb93d058ce729ba7933deb433c6
sz <sz@galacticicecube.com>
2015-05-24 23:57:35 -0500

    Fold impl into header.

--- src/lib/socket/socket_address.h
+++ src/lib/socket/socket_address.h
@@ -1,6 +1,7 @@
 /* This code is subject to the terms of the Mozilla Public License, v.2.0. http://mozilla.org/MPL/2.0/. */
 #pragma once
 
+#include <sstream>
 #include <string>
 
 class socket_address
@@ -20,4 +21,49 @@ protected:
 	std::string _address;
 	unsigned short _port;
 };
+
+inline socket_address::socket_address()
+	: _port(0)
+{
+}
+
+inline socket_address::socket_address(std::string destination)
+	: _address(std::move(destination))
+	, _port(0)
+{
+}
+
+inline socket_address::socket_address(std::string address, unsigned short port)
+	: _address(std::move(address))
+	, _port(port)
+{
+}
+
+inline const std::string& socket_address::address() const
+{
+	return _address;
+}
+
+inline unsigned short socket_address::port() const
+{
+	return _port;
+}
+
+inline std::string socket_address::toString() const
+{
+	std::stringstream str;
+	str << _address << ":" << _port;
+	return str.str();
+}
+
+inline bool socket_address::fromString(const std::string& str)
+{
+	size_t sep = str.find_last_of(':');
+	if (sep == std::string::npos || sep >= str.size()-1)
+		return false;
+
+	_address = str.substr(0, sep);
+	_port = std::stoi(str.substr(sep+1));
+	return true;
+}
 
