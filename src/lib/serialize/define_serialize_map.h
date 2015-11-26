c9b12bfd7a712a95c0f36dc35dd20bd575f295bf
sz <sz@galacticicecube.com>
2015-11-25 22:31:17 -0600

    Serialize 7 params.

--- src/lib/serialize/define_serialize_map.h
+++ src/lib/serialize/define_serialize_map.h
@@ -13,9 +13,10 @@
 #define APPLY4(t, a, b, c, d) t(a) t(b) t(c) t(d)
 #define APPLY5(t, a, b, c, d, e) t(a) t(b) t(c) t(d) t(e)
 #define APPLY6(t, a, b, c, d, e, f) t(a) t(b) t(c) t(d) t(e) t(f)
+#define APPLY7(t, a, b, c, d, e, f, g) t(a) t(b) t(c) t(d) t(e) t(f) t(g)
 
-#define NUM_ARGS_H1(dummy, x6, x5, x4, x3, x2, x1, x0, ...) x0
-#define NUM_ARGS(...) NUM_ARGS_H1(dummy, ##__VA_ARGS__, 6, 5, 4, 3, 2, 1, 0)
+#define NUM_ARGS_H1(dummy, x7, x6, x5, x4, x3, x2, x1, x0, ...) x0
+#define NUM_ARGS(...) NUM_ARGS_H1(dummy, ##__VA_ARGS__, 7, 6, 5, 4, 3, 2, 1, 0)
 #define APPLY_ALL_H3(t, n, ...) APPLY##n(t, __VA_ARGS__)
 #define APPLY_ALL_H2(t, n, ...) APPLY_ALL_H3(t, n, __VA_ARGS__)
 #define APPLY_ALL(t, ...) APPLY_ALL_H2(t, NUM_ARGS(__VA_ARGS__), __VA_ARGS__)
