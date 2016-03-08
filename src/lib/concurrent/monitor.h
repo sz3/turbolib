6fce376ffa9abd11b47ad81b4cbc93910bd2562a
sz <sz@galacticicecube.com>
2016-03-07 22:40:44 -0600

    The monitor can tell the caller whether it was a legit wakeup or not.
    timeout, cancelled = no. Otherwise = yes.

--- src/lib/concurrent/monitor.h
+++ src/lib/concurrent/monitor.h
@@ -10,17 +10,19 @@ class monitor
 public:
 	monitor()
 		: _flag(false)
+		, _cancelled(false)
 	{}
 
-	void wait()
+	bool wait()
 	{
 		std::unique_lock<std::mutex> lock(_mutex);
 		if (_flag)
 		{
 			_flag = false;
-			return;
+			return !_cancelled;
 		}
 		_cv.wait(lock);
+		return !_cancelled;
 	}
 
 	bool wait_for(unsigned ms)
@@ -31,7 +33,7 @@ public:
 			_flag = false;
 			return true;
 		}
-		return _cv.wait_for(lock, std::chrono::milliseconds(ms)) != std::cv_status::timeout;
+		return _cv.wait_for(lock, std::chrono::milliseconds(ms)) != std::cv_status::timeout && !_cancelled;
 	}
 
 	void notify_one()
@@ -46,13 +48,26 @@ public:
 
 	void signal_all()
 	{
+		signal_all_internal(false);
+	}
+
+	void cancel()
+	{
+		signal_all_internal(true);
+	}
+
+protected:
+	void signal_all_internal(bool cancelled)
+	{
 		std::unique_lock<std::mutex> lock(_mutex);
 		_flag = true;
+		_cancelled = cancelled;
 		notify_all();
 	}
 
 protected:
 	bool _flag;
+	bool _cancelled;
 	std::condition_variable _cv;
 	std::mutex _mutex;
 };
